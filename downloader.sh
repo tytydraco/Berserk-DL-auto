65e3545bbac01ff1570f8edf27d21104d8162a35
Tyler Nijmeh <tylernij@gmail.com>
2022-08-03 17:10:41 -0700

    Use output directory by default

--- downloader.sh
+++ downloader.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 echo "Starting the download of berserk chapters from https://readberserk.com/"
-curl -k -s https://readberserk.com/ | grep "btn btn-sm btn-primary mr-2" | cut -d '"' -f2 > ./working/chaplist.txt;
-for f in $(cat ./working/chaplist.txt); do
+curl -k -s https://readberserk.com/ | grep "btn btn-sm btn-primary mr-2" | cut -d '"' -f2 > ./output/chaplist.txt;
+for f in $(cat ./output/chaplist.txt); do
 	chap=$(echo $f | cut -d "/" -f5);
-	[[ -d "./working/$chap" ]] && continue
+	[[ -d "./output/$chap" ]] && continue
 	echo "Downloading $chap"
-	mkdir ./working/$chap;
-	curl -k -s $f | grep "class=\"pages__img\"" | cut -d '"' -f4 > ./working/$chap/imglist.txt;
+	mkdir ./output/$chap;
+	curl -k -s $f | grep "class=\"pages__img\"" | cut -d '"' -f4 > ./output/$chap/imglist.txt;
 	count=1;
-	for x in $(cat ./working/$chap/imglist.txt); do
+	for x in $(cat ./output/$chap/imglist.txt); do
 		case $x in
 			*".jpg")
-				curl -k -s $x > ./working/$chap/$count.jpg;
+				curl -k -s $x > ./output/$chap/$count.jpg;
 				;;
 			*".jpeg")
-				curl -k -s $x > ./working/$chap/$count.jpeg;
+				curl -k -s $x > ./output/$chap/$count.jpeg;
 				;;
 			*".png")
-				curl -k -s $x > ./working/$chap/$count.png;
+				curl -k -s $x > ./output/$chap/$count.png;
 				;;
 		esac
 		let "count=count+1";
 	done
 done
 echo "Downloads done :)"
-rm -f ./working/*.txt
-rm -f ./working/*/*.txt
-mv ./working/* ./output/
+rm -f ./output/*.txt
+rm -f ./output/*/*.txt
 echo "Files available in ./output/"
