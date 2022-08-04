e377ee1d1bd5ddb8faf8e0e60247f62d4cc21a46
Tyler Nijmeh <tylernij@gmail.com>
2022-08-03 18:34:53 -0700

    Update downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+git config --global user.email "Berserk-DL-auto@github-action.com"
+git config --global user.name "Berserk-DL-auto"
+
 echo "Starting the download of berserk chapters from https://readberserk.com/"
 curl -k -s https://readberserk.com/ | grep "btn btn-sm btn-primary mr-2" | cut -d '"' -f2 > ./output/chaplist.txt;
 for f in $(cat ./output/chaplist.txt); do
@@ -22,6 +26,11 @@ for f in $(cat ./output/chaplist.txt); do
 		esac
 		let "count=count+1";
 	done
+	
+	
+  	git add .
+  	git commit -sam "Updated chapter: $chap"
+  	git push
 done
 echo "Downloads done :)"
 rm -f ./output/*.txt
