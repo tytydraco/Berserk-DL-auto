090e74903fc5b597624a9b956f7a7960a3525a50
Tyler Nijmeh <tylernij@gmail.com>
2022-10-16 11:46:02 -0700

    Try to fix bad downloads

--- downloader.sh
+++ downloader.sh
@@ -6,23 +6,24 @@ git config --global user.name "Berserk-DL-auto"
 echo "Starting the download of berserk chapters from https://readberserk.com/"
 curl -k -s https://readberserk.com/ | grep "btn btn-sm btn-primary mr-2" | cut -d '"' -f2 > ./output/chaplist.txt;
 for f in $(cat ./output/chaplist.txt); do
-	chap=$(echo $f | cut -d "/" -f5);
+	chap=$(echo "$f" | cut -d "/" -f5);
 	echo "Downloading $chap"
-	rm -rf ./output/$chap;
-	mkdir -p ./output/$chap;
-	curl -k -s $f | grep "class=\"pages__img\"" | cut -d '"' -f4 > ./output/$chap/imglist.txt;
+	rm -rf "./output/$chap";
+	mkdir -p "./output/$chap";
+	curl -k -s $f | grep "class=\"pages__img\"" | cut -d '"' -f4 > "./output/$chap/imglist.txt";
 	
 	count=1;
-	for x in $(cat ./output/$chap/imglist.txt); do
+	for x in $(cat "./output/$chap/imglist.txt"); do
+                echo "Fetching $x"
 		case $x in
 			*".jpg"*)
-				curl -k -s "$x" > ./output/$chap/$count.jpg;
+				curl -L -k -s "$x" > "./output/$chap/$count.jpg";
 				;;
 			*".jpeg"*)
-				curl -k -s "$x" > ./output/$chap/$count.jpeg;
+				curl -L -k -s "$x" > "./output/$chap/$count.jpeg";
 				;;
 			*".png"*)
-				curl -k -s "$x" > ./output/$chap/$count.png;
+				curl -L -k -s "$x" > "./output/$chap/$count.png";
 				;;
 		esac
 		let "count=count+1";
@@ -33,6 +34,6 @@ for f in $(cat ./output/chaplist.txt); do
   	git push
 done
 echo "Downloads done :)"
-rm -f ./output/*.txt
-rm -f ./output/*/*.txt
+rm -f "./output/*.txt"
+rm -f "./output/*/*.txt"
 echo "Files available in ./output/"
