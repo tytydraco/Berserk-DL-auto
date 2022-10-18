6410d85d7e89ac3e84b9e759c8b81500538686d7
Berserk-DL-auto <Berserk-DL-auto@github-action.com>
2022-10-18 16:46:32 -0700

    Refactor entire downloader
    Signed-off-by: Berserk-DL-auto <Berserk-DL-auto@github-action.com>

--- downloader.sh
+++ downloader.sh
@@ -1,39 +1,41 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+get_chapter_list() {
+	curl -k -s "https://readberserk.com/" \
+	| grep "btn btn-sm btn-primary mr-2" \
+	| cut -d '"' -f2
+}
+
+get_image_list() {
+	curl -k -s "$chapter_url" \
+		| grep "class=\"pages__img\"" \
+		| cut -d '"' -f4
+}
 
 git config --global user.email "Berserk-DL-auto@github-action.com"
 git config --global user.name "Berserk-DL-auto"
 
-echo "Starting the download of berserk chapters from https://readberserk.com/"
-curl -k -s https://readberserk.com/ | grep "btn btn-sm btn-primary mr-2" | cut -d '"' -f2 > ./output/chaplist.txt;
-for f in $(cat ./output/chaplist.txt); do
-	chap=$(echo "$f" | cut -d "/" -f5);
-	echo "Downloading $chap"
-	rm -rf "./output/$chap";
-	mkdir -p "./output/$chap";
-	curl -k -s $f | grep "class=\"pages__img\"" | cut -d '"' -f4 > "./output/$chap/imglist.txt";
-	
+echo "Downloading chapter list..."
+chapter_list="$(get_chapter_list)"
+for chapter_url in $chapter_list; do
+	chapter_id=$(echo "$chapter_url" | cut -d "/" -f5);
+	echo "Downloading $chapter_id..."
+
+	rm -rf "./output/$chapter_id";
+	mkdir -p "./output/$chapter_id";
+
+	image_list="$(get_image_list "$chapter_id")"
 	count=1;
-	for x in $(cat "./output/$chap/imglist.txt"); do
-                echo "Fetching $x"
-		case $x in
-			*".jpg"*)
-				curl -L -k -s "$x" > "./output/$chap/$count.jpg";
-				;;
-			*".jpeg"*)
-				curl -L -k -s "$x" > "./output/$chap/$count.jpeg";
-				;;
-			*".png"*)
-				curl -L -k -s "$x" > "./output/$chap/$count.png";
-				;;
-		esac
-		let "count=count+1";
+	for image_url in $image_list; do
+		filename="$(echo "$image_url" | sed 's|.*/||g' | sed 's/\?.*$//g')"
+		extension="${filename##*.}"
+		echo "Fetching $filename"
+		curl -Lks "$image_url" > "./output/$chapter_id/$count.$extension"
+		((count+=1))
 	done
-	
+
   	git add .
-  	git commit -sam "Updated chapter: $chap"
-  	git push
+  	git commit -sam "Updated chapter: $chapter_id" && git push
 done
+
 echo "Downloads done :)"
-rm -f "./output/*.txt"
-rm -f "./output/*/*.txt"
-echo "Files available in ./output/"
