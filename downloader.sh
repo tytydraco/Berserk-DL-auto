2b73aba2184b64f16ce937079f3ae3a3bd81e73b
Berserk-DL-auto <Berserk-DL-auto@github-action.com>
2022-10-18 17:23:30 -0700

    Update base repo again
    Signed-off-by: Berserk-DL-auto <Berserk-DL-auto@github-action.com>

--- downloader.sh
+++ downloader.sh
@@ -12,6 +12,9 @@ get_image_list() {
 		| cut -d '"' -f4
 }
 
+git config --global user.email "Berserk-DL-auto@github-action.com"
+git config --global user.name "Berserk-DL-auto"
+
 echo "Downloading chapter list..."
 chapter_list="$(get_chapter_list)"
 while IFS= read -r chapter_url_raw; do
@@ -32,6 +35,9 @@ while IFS= read -r chapter_url_raw; do
 		curl -LSks "$image_url" > "./output/$chapter_id/$count.$extension"
 		((count+=1))
 	done <<< "$image_list"
+
+	git add .
+  	git commit -sam "Updated chapter: $chapter_id" && git push
 done <<< "$chapter_list"
 
 echo "Downloads done :)"
